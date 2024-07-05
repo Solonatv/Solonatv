7661bcfd61fedc3cc4b8ce8473f33ee2ee1e3137
diemazt25 <45594645+diemazt25@users.noreply.github.com>
2024-07-06 00:41:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ document.oncontextmenu = () => {
 	return false
 }
 
+/* Still anyone can inspect element by F12 key. view page source by ctrl + u key. Let's prevent these*/
+
 document.onekeydown = e => {
 	//Prevent F12 key
 	if(e.key == "F12") {
 		Alert(Jangan tekan F12 yah")
 		return false
 	}
-
-	//Prevent F12 key
+	//Prevent showing page source by ctrl + u
 	if(e.ctrlkey && e.key == "u") {
 		Alert(Jangan tekan tekan ctrl + u yah")
 		return false
